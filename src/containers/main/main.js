51319ad8d2a4697ae72f4a6b742fdedc9b5c2a3d
Noam Silver <noamsilver@gmail.com>
2019-02-05 23:33:06 +0200

    Now the scrolling is infinite as long as there are more pages to fetch. When changing the search term the page is scrolled back to the top. Changed all the fetch methods to be async. Using throttle to limit the amount of calls to handleScrollChange.

--- src/containers/main/main.js
+++ src/containers/main/main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import EventEmmiter from 'events';
+import throttle from '../../utils/throttle';
 import Header from '../header';
 import ResultsPanel from '../results-panel';
 import { flickrAllImages, flickrImageSearch } from '../../api/search-api';
@@ -10,42 +11,94 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentSearch: undefined,
       list: [],
       wasSearchPerformed: false,
+      changedSearch: false,
+      lastPage: 0,
     };
     
+    this.handleScrollChangeThrottled = throttle(this.handleScrollChange, 500)
     this.getAllImages();
   };
 
-  getAllImages = () => {
+  getAllImages = async () => {
     console.log('in getAllImages');
-    flickrAllImages().then((list) => {
-      console.log({allList: list});
-      this.setState(() => ({
-        list: list.photos.photo,
-        wasSearchPerformed: true,
-      }));
-    });
+    this.setState(() => ({ lastPage: 1 }))
+    const currentSearch = await flickrAllImages();
+    currentSearch.text = '';
+    console.log({currentSearchAllList: currentSearch});
+    this.setState(() => ({
+      currentSearch,
+      list: currentSearch.photos.photo,
+      wasSearchPerformed: true,
+      changedSearch: true,
+    }));
   };
 
-  handleSearchChange = (text) => {
+  handleSearchChange = async (text) => {
     console.log('in handleSearchChange', {text});
     if (text === '') {
       this.getAllImages();
     } else {
-      flickrImageSearch(text).then((list) => {
-        console.log({searchList: list})
-        this.setState(() => ({
-          list: list.photos.photo,
-          wasSearchPerformed: true,
-        }))
-      });
+      this.setState(() => ({ lastPage: 1 }))
+      const currentSearch = await flickrImageSearch(text, 1);
+      currentSearch.text = text;
+      console.log({currentSearch});
+      this.setState(() => ({
+        currentSearch, 
+        list: currentSearch.photos.photo,
+        wasSearchPerformed: true,
+        changedSearch: true,
+      }));
     };
   };
 
   onClick = () => {
     events.emit('galleryClick');
-  }
+  };
+
+  handleScrollChange = async () => {
+    console.log('in handleScrollChange', {
+      scrollY: window.scrollY,
+      height: document.body.clientHeight,
+    });
+    const { currentSearch, lastPage } = this.state;
+    const { page, pages } = this.state.currentSearch.photos;
+    const { scrollY } = window;
+    const { clientHeight } = document.body;
+    console.log({body: document.body, clientHeight, clientHeight03: clientHeight * 0.3, clientMinus7000: clientHeight - 7000, page, pages});
+    console.log({page, pages, lastPage});
+    const threshold = clientHeight > 10000 ? clientHeight - 7000 : clientHeight * 0.3;
+    if (scrollY > threshold && page < pages && lastPage < page + 1) {
+      this.setState(() => ({ lastPage: page + 1 }));
+      const newCurrentSearch = await flickrImageSearch(currentSearch.text, page + 1);
+      newCurrentSearch.text = currentSearch.text;
+      console.log({newCurrentSearch, currentSearch: this.state.currentSearch});
+      this.setState(state => ({
+        currentSearch: newCurrentSearch,
+        list: [
+          ...state.list,
+          ...newCurrentSearch.photos.photo,
+        ]
+      }));
+    };
+  };
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.handleScrollChangeThrottled, false);
+  };
+
+  componentDidUpdate = () => {
+    if (this.state.changedSearch) {
+      window.scrollTo(0, 0);
+      this.setState(() => ({changedSearch: false}))
+    }
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this.handleScrollChangeThrottled, false);
+  };
 
   render() {
     console.log('in Main render');
